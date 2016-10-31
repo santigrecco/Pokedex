2b6d207c50276e35031f68ff43e6bde706e2f23d
Facundo Ferreira La Valle <facundo.ferreira@globant.com>
2016-10-31 15:38:07 -0300

    archivo 1 de 3

--- Proyecto Pokedex/src/HelloWorld.jsx
+++ Proyecto Pokedex/src/HelloWorld.jsx
@@ -1,41 +1,43 @@
 import React,{ Component } from 'react';
-import {PokemonView,searchPokemonById, searchPokemonByType,funcionDelMap} from './PokemonView';
-
-
+import {PokemonView} from './PokemonView';
+import IngresarNombre from './IngresarNombre.jsx';
+import IngresarTipos from './IngresarTipos.jsx';
 
 export class Principal extends Component{
     constructor(props){
         super(props);
         this.state = {
         	"pokemons":[],
-        	"pokemonABuscar": "pikachu",
           "cargando": false,
           "mensajeResultado": "",
           "criterioDeBusqueda": "id"
         };
        	// searchPokemonById = searchPokemonById.bind(this);
-       	this.actualizarPokemonABuscar = this.actualizarPokemonABuscar.bind(this);
+       	
        	this.drawView = this.drawView.bind(this);
-        this.onBuscar = this.onBuscar.bind(this);
-        this.onBuscarPorTipo = this.onBuscarPorTipo.bind(this);
-        this.onBuscarAlAzar = this.onBuscarAlAzar.bind(this);
+        this.actualizarCriterio = this.actualizarCriterio.bind(this);
+        this.drawOpcionesBusqueda = this.drawOpcionesBusqueda.bind(this);
+        this.handleStateCargando = this.handleStateCargando.bind(this);
+        this.handleStatePokemons = this.handleStatePokemons.bind(this);
+        this.getStateCargando = this.getStateCargando.bind(this);
     }
 
-    componentWillMount() {
-        const {pokemonABuscar} = this.state;
-        const {onBuscar} = this;
-
-
-        // onBuscar(pokemonABuscar);
+    handleStateCargando(cargando){
+      this.setState({
+        cargando: cargando
+      });
     }
 
-    actualizarPokemonABuscar(event){
-    	const {value} = event.target;
-    	const {pokemonABuscar} = this.state;
-    	this.setState(
-    		{pokemonABuscar: value.toLowerCase()}
-    	);
-    }   
+    handleStatePokemons(pokemons){
+      this.setState({
+        pokemons: pokemons
+      });
+    }
+
+    getStateCargando(){
+      return this.state.cargando;
+    }
+
 
     drawView(pokemons){
       if (pokemons.length > 0) {
@@ -63,90 +65,41 @@ export class Principal extends Component{
         }
     }
 
-    onBuscar(){
-      let {pokemons,pokemonABuscar,cargando} = this.state;
+    actualizarCriterio(nuevoCriterio){
+      console.log("nuevo criterio = "+nuevoCriterio)
       this.setState({
-        cargando: true
-      })
-      searchPokemonById(pokemonABuscar)
-      .then( (pokemonData) => {
-        if(pokemonData[0].detail != "Not found." ){
-          //pokemons.push(pokemonData);
-        this.setState({
-          pokemons: pokemonData
-        });
-      }else{
-        console.log("No se encontro el pokemon");
-      }
-      this.setState({
-        cargando: false
-      })
-      });
-
-
-   
-    }
-
-    onBuscarPorTipo(){
-      let {pokemons,pokemonABuscar,cargando} = this.state;
-      this.setState({
-        cargando: true
-      })
-      searchPokemonByType(pokemonABuscar)
-      .then( (pokemonData) => {
-
-      if(pokemonData.detail != "Not found." ){
-          //pokemons.push(pokemonData);
-        this.setState({
-          pokemons: pokemonData
-        });
-      }else{
-        console.log("No se encontro el pokemon");
-      }
-      this.setState({
-          cargando: false
-        });
-
-      });
-    }
-    onBuscarAlAzar(){
-      let {pokemons,cargando} = this.state;
-      let random = Math.floor(Math.random() * (720 ) + 1.99);
-      this.setState({
-        cargando: true
-      })
-      searchPokemonById(random)
-      .then( (pokemonData) => {
-        if(pokemonData[0].detail != "Not found." ){
-          //pokemons.push(pokemonData);
-        this.setState({
-          pokemons: pokemonData
-        });
-      }else{
-        console.log("No se encontro el pokemon");
-      }
-      this.setState({
-        cargando: false
-      })
+        criterioDeBusqueda: nuevoCriterio
       });
     }
 
-    elegirBusqueda(){
-      let {criterioDeBusqueda} = this.state;
-      let {onBuscar,onBuscarPorTipo,onBuscarAlAzar} = this;
+    drawOpcionesBusqueda(){
+      let {criterioDeBusqueda, cargando, pokemons} = this.state;
+      const{handleStatePokemons, handleStateCargando, getStateCargando}=this;
+      
       if (criterioDeBusqueda=='id'){
-        onBuscar();
+        console.log("entro a ID");
+        return(
+            <IngresarNombre
+              handleStateCargando = {handleStateCargando}
+              handleStatePokemons = {handleStatePokemons}
+              getStateCargando = {getStateCargando}
+            />
+          );
       }else if(criterioDeBusqueda=='tipo'){
-        onBuscarPorTipo();
-      }else if (criterioDeBusqueda=='alAzar'){
-        onBuscarAlAzar();
+        return (<IngresarTipos
+          handleStateCargando = {handleStateCargando}
+          handleStatePokemons = {handleStatePokemons}
+          getStateCargando = {getStateCargando}
+        />);
       }
+      console.log('entro a opbusq');
     }
-    //bas exp nombre y peso
+
+
     render(){
         const {name} = this.props;
         const {pokemons, pokemonABuscar,cargando} = this.state;
-        const {drawView,onBuscarAlAzar,actualizarPokemonABuscar,onBuscar,onBuscarPorTipo} = this;
+        const {drawView,drawOpcionesBusqueda,onBuscarAlAzar,actualizarPokemonABuscar,onBuscar,onBuscarPorTipo,elegirBusqueda,actualizarCriterio} = this;
         let style = {};
         if (!cargando) {
           style = {
@@ -159,68 +112,24 @@ export class Principal extends Component{
         }
       	
       	return(
-
       		<div>
-
-      			<input type="text" onChange={actualizarPokemonABuscar} />
+            <div>
+              <span>Criterio busqueda</span>
+              <button onClick={() => actualizarCriterio('id')}>Buscar por Nombre</button>
+              <button onClick={() => actualizarCriterio('tipo')}>Buscar por Tipo</button>
+            </div>
             <img src="http://66.media.tumblr.com/74da2c5713f820807e9aaceb44923bcb/tumblr_oaocdr1gOk1rymyweo1_500.gif" style={style}/>
             <br/>
-            <select className="">
-              <option value="nombre">Nombre</option>
-              <option value="tipo">Tipo</option>
-              <option value="alAzar">¡Al azar!</option>
-            </select>
-            <select className="Lista">
-              <option value="cualquiera">Cualquiera</option>
-              <option value="Fire">Fire</option>
-              <option value="Water">Water</option>
-              <option value="Grass">Grass</option>
-              <option value="Electric">Electric</option>
-              <option value="Bug">Bug</option>
-              <option value="Dark">Dark</option>
-              <option value="Dragon">Dragon</option>
-              <option value="Fairy">Fairy</option>
-              <option value="Fighting">Fighting</option>
-              <option value="Flying">Flying</option>
-              <option value="Ghost">Ghost</option>
-              <option value="Ground">Ground</option>
-              <option value="Ice">Ice</option>
-              <option value="Normal">Normal</option>
-              <option value="Poison">Poison</option>
-              <option value="Psychic">Psychic</option>
-              <option value="Rock">Rock</option>
-              <option value="Steel">Steel</option>
-            </select>
-             <select>
-              <option value="cualquiera">Cualquiera</option>
-              <option value="Fire">Fire</option>
-              <option value="Water">Water</option>
-              <option value="Grass">Grass</option>
-              <option value="Electric">Electric</option>
-              <option value="Bug">Bug</option>
-              <option value="Dark">Dark</option>
-              <option value="Dragon">Dragon</option>
-              <option value="Fairy">Fairy</option>
-              <option value="Fighting">Fighting</option>
-              <option value="Flying">Flying</option>
-              <option value="Ghost">Ghost</option>
-              <option value="Ground">Ground</option>
-              <option value="Ice">Ice</option>
-              <option value="Normal">Normal</option>
-              <option value="Poison">Poison</option>
-              <option value="Psychic">Psychic</option>
-              <option value="Rock">Rock</option>
-              <option value="Steel">Steel</option>
-            </select>
-            <button onClick={() => elegirBusqueda()}>Buscar</button>
-      			<button onClick={ () => onBuscar()}>Buscar por Nombre </button>
-            <button onClick={ () => onBuscarPorTipo()}>Buscar por Tipo </button>
-            <button onClick={ () => onBuscarAlAzar()}>Random</button><br/>
       			<div>
       				{
-				 		     drawView(pokemons)
+                 drawOpcionesBusqueda()				 		     
       				}
       			</div>
+            <div>
+              {
+                 drawView(pokemons)
+              }
+            </div>
       		</div>
       	);
     }
